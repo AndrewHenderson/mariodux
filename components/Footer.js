0cfe2d22a14db2cb97ad321c6000451ecd40a39f
andrewhenderson <andrew.m.henderson@gmail.com>
2016-05-13 08:10:17 -0700

    Footer link styling

--- components/Footer.js
+++ components/Footer.js
@@ -3,14 +3,24 @@ define(function(require) {
   'use strict';
 
   var Marionette = require('marionette');
+  var store = require('store');
 
   return Marionette.LayoutView.extend({
 
     template: function() {
-      return '<p>Show: <a href="#SHOW_ALL">All</a>'
-      + '<a href="#SHOW_ACTIVE">Active</a>'
-      + '<a href="#SHOW_COMPLETED">Completed</a>'
-      + '</p>';
+
+      var state = store.getState();
+      var visibilityFilter = state.visibilityFilter;
+
+      var showAll = visibilityFilter !== 'SHOW_ALL' ? '<a href="#SHOW_ALL">All</a>' : '<span>All</span>';
+      var showActive = visibilityFilter !== 'SHOW_ACTIVE' ? '<a href="#SHOW_ACTIVE">Active</a>' : '<span>Active</span>'
+      var showCompleted = visibilityFilter !== 'SHOW_COMPLETED' ? '<a href="#SHOW_COMPLETED">Completed</a>' : '<span>Completed</span>'
+
+      return '<p>Show: '
+        + showAll
+        + showActive
+        + showCompleted
+        + '</p>';
     }
   });
 });
