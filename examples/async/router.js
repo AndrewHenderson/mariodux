90c4f2d07b6d6bf6b1ec5fc5b015e94bc98add15
andrewhenderson <andrew.m.henderson@gmail.com>
2016-05-20 01:04:49 -0700

    Removed unused route

--- examples/async/router.js
+++ examples/async/router.js
@@ -1,18 +1,6 @@
 define(function(require) {
 
   var Backbone = require('backbone');
-  var store = require('store');
-  var dispatch = store.dispatch;
-  var setVisibilityFilter = require('actions/index').setVisibilityFilter;
 
-  return Backbone.Router.extend({
-
-    routes: {
-      ':filter': 'filter'
-    },
-
-    filter: function (filter) {
-      dispatch(setVisibilityFilter(filter));
-    }
-  });
+  return Backbone.Router.extend({});
 });
